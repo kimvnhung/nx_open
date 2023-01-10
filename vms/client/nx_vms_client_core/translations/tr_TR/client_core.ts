e9f86118ebe9027587c9f8449d0e83311a7fd0de
Jenkins <jenkins@networkoptix.com>
2023-01-10 00:31:36 +0000

    Updated translations (committed by Jenkins)
    Branch: mobile_22.4
    
    https://jenkins.ru.nxteam.dev/job/nightly.mobile.mobile_22_4.update-translation/151/

--- vms/client/nx_vms_client_core/translations/tr_TR/client_core.ts
+++ vms/client/nx_vms_client_core/translations/tr_TR/client_core.ts
@@ -90,7 +90,7 @@
     </message>
     <message>
       <source>Invalid certificate</source>
-      <translation type="unfinished">Invalid certificate</translation>
+      <translation>Geçersiz sertifika</translation>
     </message>
     <message>
       <source>The server&amp;apos;s certificate was rejected.</source>
