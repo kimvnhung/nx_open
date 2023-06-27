ee613c7ebfe5be9207c48b6fc64cc70e4393eee6
Jenkins <jenkins@networkoptix.com>
2023-06-27 00:53:38 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_5.0_patch
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_5_0_patch.update-translation/150/

--- vms/client/nx_vms_client_desktop/translations/ja_JP/client_base.ts
+++ vms/client/nx_vms_client_desktop/translations/ja_JP/client_base.ts
@@ -5851,7 +5851,7 @@ If you intended to move analytics data to another storage location, please conta
     </message>
     <message>
       <source>Login cannot be empty.</source>
-      <translation>ログイン情報を入力してください</translation>
+      <translation>ログイン名を入力してください。</translation>
     </message>
     <message>
       <source>User with specified login already exists.</source>
