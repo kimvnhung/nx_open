d1a8a3e1c892d6044362382d2826f983a0fc1e3d
Jenkins <jenkins@networkoptix.com>
2023-09-20 00:37:40 +0000

    Updated translations (committed by Jenkins)
    Branch: vms_5.1.1
    
    https://jenkins.us.nxteam.dev/job/nightly.vms.vms_5_1_1.update-translation/20/

--- vms/client/nx_vms_client_desktop/translations/vi_VN/client_qml.ts
+++ vms/client/nx_vms_client_desktop/translations/vi_VN/client_qml.ts
@@ -799,21 +799,21 @@
     </message>
     <message>
       <source>Server is offline</source>
-      <translation>Máy chủ đang ẩn</translation>
+      <translation>Máy chủ đang ngoại tuyến</translation>
     </message>
   </context>
   <context>
     <name>PolyFigure</name>
     <message>
       <source>Delete</source>
-      <translation>Xóa bỏ</translation>
+      <translation>Xoá</translation>
     </message>
   </context>
   <context>
     <name>Polygon</name>
     <message>
       <source>Polygon is not valid. Remove self-intersections to proceed.</source>
-      <translation>Lỗi đa giác. Xóa phần sai để tiếp tục.</translation>
+      <translation>Đa giác không hợp lệ. Loại bỏ các nút giao nhau để tiếp tục.</translation>
     </message>
     <message numerus="yes">
       <source>This polygon requires at least %n points.</source>
@@ -828,7 +828,7 @@
     <message numerus="yes">
       <source>The maximum number of points has been reached (%n points).</source>
       <translation>
-        <numerusform>Số điểm tối đa đã đạt được (%n điểm).</numerusform>
+        <numerusform>Đã đạt số điểm tối đa (%n điểm).</numerusform>
       </translation>
     </message>
   </context>
@@ -836,7 +836,7 @@
     <name>Preview</name>
     <message>
       <source>NO DATA</source>
-      <translation>Không có sẵn</translation>
+      <translation>KHÔNG CÓ DỮ LIỆU</translation>
     </message>
   </context>
   <context>
@@ -851,7 +851,7 @@
     </message>
     <message>
       <source>Show on Layout</source>
-      <translation>Hiển thị trên Layout</translation>
+      <translation>Hiển thị trên Bố cục</translation>
     </message>
     <message>
       <source>Preview is not available for the selected object</source>
@@ -862,7 +862,7 @@
     <name>PromoPageButtons</name>
     <message>
       <source>OK</source>
-      <translation>Xác nhận</translation>
+      <translation>OK</translation>
     </message>
     <message>
       <source>Next</source>
